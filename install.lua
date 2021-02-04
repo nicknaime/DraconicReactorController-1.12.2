f8cd3d4bc7b1da877fc4c2ac063df244c130b11d
nicknaime <37012322+nicknaime@users.noreply.github.com>
2021-02-04 14:14:57 -0500

    Update install.lua
    Changed libURL

--- install.lua
+++ install.lua
@@ -2,7 +2,7 @@
 --rhNbNbFE
 --Version 1.0
 
-local libURL = "https://raw.githubusercontent.com/OmegaPhi115/Draconic-Evolution-Reactor-Controller/master/lib/f.lua"
+local libURL = "https://raw.githubusercontent.com/nicknaime/DraconicReactorController-1.12.2/1.0/lib/f.lua"
 local startupURL = "https://raw.githubusercontent.com/OmegaPhi115/Draconic-Evolution-Reactor-Controller/master/Draconic-Evolution-Reactor-Controller.lua"
 local lib, startup
 local libFile, startupFile
